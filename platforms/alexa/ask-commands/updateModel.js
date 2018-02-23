6d4142f79b2641b6452c5ed0dfb1eef207755129
gberk <gberkowitz@logmein.com>
2018-02-23 07:02:45 -0500

    Remove unnecessary console logs and update package version

--- platforms/alexa/ask-commands/updateModel.js
+++ platforms/alexa/ask-commands/updateModel.js
@@ -11,7 +11,6 @@ var exec = require('child-process-promise').exec;
 
                     */
 var updateModel = function(ayvaConfig){
-    console.log(ayvaConfig)
     var commandText = `ask api update-model -s ${ayvaConfig.alexa.skillId} -f ./en-US.json -l en-US`;
     exec(commandText)
         .then((data) =>  { 
@@ -27,8 +26,7 @@ var updateModel = function(ayvaConfig){
         .catch((err) => { 
             console.log(err.stderr);
             fs.unlink('./en-US.json', function(data,err){
-                if(!err)
-                    console.log("Successful deploy to Alexa")
+                return
             })
         } )  
 }
