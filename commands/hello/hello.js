a9d113a4cc2f96705135ce095633d0018bc70de1
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-03-05 22:52:33 -0800

    ux for hello

--- commands/hello/hello.js
+++ commands/hello/hello.js
@@ -6,17 +6,38 @@ inquirer = require('inquirer'),
 init = require('../init/init')
 p = require('path');
 
-var walkthrough = function(installPath) {
-    var config = Ayva.Empty;
-    installPath = installPath || 'ayva-helloWorld'
+// UX
+var clear = require('clear'),
+    figlet = require('figlet'),
+    chalk = require('chalk')
 
-    var cloneHelloWorld = `git clone --depth=1 https://github.com/rflabs/ayva-helloWorld.git ${installPath} && rm -rf ${installPath}/.git`; //Don't use original repo
-    
-    console.log('Cloning repo from git...')
-    exec(cloneHelloWorld, "", function(err, data){
-        if(err) return console.log("Hello failed: Folder already exists at the specified path. Try deleting or specifying another path")
-        init(installPath)
-    });
+var walkthrough = function(installPath) {
+    clear();
+    var outputFormat = {
+        font: 'Graffiti',
+        horizontalLayout: 'full',
+        verticalLayout: 'full'
+    }
+
+    figlet.text('Ayva', outputFormat, function(err, ascii) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(chalk.rgb(200,200,90)(ascii));
+        console.log("\n")
+        
+        var config = Ayva.Empty;
+        installPath = installPath || 'ayva-helloWorld'
+
+        var cloneHelloWorld = `git clone --depth=1 https://github.com/rflabs/ayva-helloWorld.git ${installPath} && rm -rf ${installPath}/.git`; //Don't use original repo
+        
+        console.log('Cloning repo from git...')
+        exec(cloneHelloWorld, "", function(err, data){
+            if(err) return console.log("Hello failed: Folder already exists at the specified path. Try deleting or specifying another path")
+            init(installPath)
+        })
+    })
 }
 
 module.exports = walkthrough;
