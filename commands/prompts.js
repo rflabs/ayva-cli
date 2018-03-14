364ac8a3e78ab6e451afc435d9636067a7815d2f
gberk <gavin.berkowitz.bu@gmail.com>
2018-03-14 13:11:02 -0400

    Remove extra text

--- commands/prompts.js
+++ commands/prompts.js
@@ -10,7 +10,7 @@ var formatAsError = function(text){
 
 var chooseYourOwnAdventure = {
   name: 'platform',
-  message: formatAsMainText("Welcome to Ayva! Which platform(s) would you like to configure for this project? (You can always add more later)"),
+  message: formatAsMainText("Welcome to Ayva! Which platform(s) would you like to configure for this project?"),
   type: 'list',
   prefix: "-",
   default: 'Google (Dialogflow)',
