0b897c2a1e061f86ca45a271c68f7878edf19b41
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2018-03-17 12:04:25 -0400

    Cross platform delete folder for git removal on ayva create

--- commands/create/create.js
+++ commands/create/create.js
@@ -4,6 +4,8 @@ var p = require('path'),
     prompts = require('../prompts.js'),
     inquirer = require('inquirer'),
     init = require('../init/init'),
+    rimraf = require('rimraf'),
+    path = require('path'),
     Ayva = require('../ayvaConfigProvider')
 
 // UX
@@ -31,7 +33,6 @@ var walkthrough = function(installPath) {
         if(!installPath) 
             return console.log(prompts.formatAsError("Project name undefined. Please provide a project name using the syntax ayva create projectName or ayva create /path/to/projectName"))
         
-        let cloneHelloWorld = `git clone --depth=1 https://github.com/rflabs/ayva-helloWorld.git ${installPath} && rm -rf ${installPath}/.git`; //Don't use original repo
         
         //Auto generate "project name" from end of supplied path, and use it as invocation phrase
         Ayva.loadConfig(installPath).then(c => {
@@ -39,10 +40,16 @@ var walkthrough = function(installPath) {
             let pathSplit = installPath.split(p.sep)
             ayvaConfig.invocationPhrase = pathSplit[pathSplit.length-1]
 
-            exec(cloneHelloWorld, "", function(err, data){
+            let cloneEmptyProject = `git clone --depth=1 https://github.com/rflabs/ayva-helloWorld.git ${installPath}`
+
+            exec(cloneEmptyProject, "", function(err, data){
                 if(err) return console.log(prompts.formatAsError("Create failed: Folder already exists at the specified path. Try deleting or specifying another path"))
-                Ayva.saveConfig(installPath, ayvaConfig)
-                init(installPath)
+
+                rimraf(path.join(installPath, '/.git'), function(err){
+                    if(err) return console.log(err);
+                    Ayva.saveConfig(installPath, ayvaConfig)
+                    init(installPath)
+                })
             })
         })
     })
